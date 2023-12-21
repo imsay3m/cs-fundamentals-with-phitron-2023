87100333c9e3b929212b84f95bc286c163a15dc3
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2023-12-22 00:22:51 +0600

    assignment-03 ADD

--- semester-03/software-dev-project/week-03/module-12/venv_12/restaurant/meal/urls.py
+++ semester-03/software-dev-project/week-03/module-12/venv_12/restaurant/meal/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from .views import base
+from django.urls import path
+from .views import index,items
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',base,name='base'),
-    path('meal/',include('meal.urls')),
-    path('about/',include('about_us.urls'))
+    path('',index,name='index'),
+    path('items/',items,name='items')
 ]
--- semester-03/software-dev-project/week-03/module-12/venv_12/restaurant/restaurant/settings.py
+++ semester-03/software-dev-project/week-03/module-12/venv_12/restaurant/restaurant/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'meal',
+    'about_us',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
