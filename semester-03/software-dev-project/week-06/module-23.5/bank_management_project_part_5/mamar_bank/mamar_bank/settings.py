5a350a5df0167c6943589fca6bfd1f50e9e880bd
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2024-01-25 23:58:57 +0600

    week-06 practice COMPLETED

--- semester-03/software-dev-project/week-06/module-23.5/bank_management_project_part_5/mamar_bank/mamar_bank/settings.py
+++ semester-03/software-dev-project/week-06/module-23.5/bank_management_project_part_5/mamar_bank/mamar_bank/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -140,8 +140,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = env("MAIL_USER")
